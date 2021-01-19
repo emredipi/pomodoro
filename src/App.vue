bb5c942dc822f29aca68d35f9b38619b29f2b398
emredipi <mail@emredipi.com>
2021-01-19 23:30:33 +0300

    added card component

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,18 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <router-view/>
   </div>
 </template>
 
-<script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
-</script>
-
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+  width: 100%;
+}
+body{
+  background-color: #1d1d1d;
 }
 </style>
---
+++ src/components/Card.vue
@@ -0,0 +1,52 @@
+<template>
+  <div class="card">
+    <div class="header" v-if="!!$slots['header']">
+      <slot name="header"></slot>
+    </div>
+    <div class="body">
+      <slot/>
+    </div>
+    <div class="footer" v-if="!!$slots['footer']">
+      <slot name="footer"/>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Card"
+}
+</script>
+
+<style scoped>
+.card {
+  background-color: #111111;
+  border-radius: 5px;
+  width: 100%;
+  height: 100%;
+  max-height: 500px;
+  max-width: 500px;
+  color: #c6c6c6;
+  display: flex;
+  flex-direction: column;
+}
+
+.header, .footer {
+  padding: 10px;
+  font-weight: 600;
+  background-color: black;
+  opacity: 1;
+}
+
+.header {
+  border-radius: 5px 5px 0 0;
+}
+
+.footer {
+  border-radius: 0 0 5px 5px;
+}
+
+.body {
+  flex: 1;
+}
+</style>
