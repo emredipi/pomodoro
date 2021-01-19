43a148f04aa574e2b40c08d0086f338b2fc3767c
emredipi <mail@emredipi.com>
2021-01-20 00:21:12 +0300

    small and round styles for button

--- src/components/PButton.vue
+++ src/components/PButton.vue
@@ -1,17 +1,18 @@
 <template>
-  <Button>
+  <Button :class="{small:small!==undefined,round:round!==undefined}">
     <slot/>
   </Button>
 </template>
 
 <script>
 export default {
-  name: "PButton"
+  name: "PButton",
+  props: ['small', 'round']
 }
 </script>
 
 <style scoped>
-button{
+button {
   border: 0;
   padding: 10px 15px;
   background-color: #0c0c0c;
@@ -19,10 +20,21 @@ button{
   border-radius: 5px;
   font-size: 2rem;
   transition: all 100ms;
+  outline: none;
 }
-button:hover{
+
+button:hover {
   cursor: pointer;
   background: black;
-  color:white;
+  color: white;
+}
+
+.small {
+  font-size: 1rem;
+  padding: 5px 10px;
+}
+
+.round {
+  border-radius: 50%;
 }
 </style>
