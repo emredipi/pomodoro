d831dc37618e47ed5d21335b97117652d7614dfb
emredipi <mail@emredipi.com>
2021-01-19 23:30:21 +0300

    added vue router

---
+++ src/views/Home.vue
@@ -0,0 +1,37 @@
+<template>
+  <Card>
+    <div slot="header">Header</div>
+    <div class="center">
+      <div class="time">
+        10:00
+      </div>
+    </div>
+  </Card>
+</template>
+
+<script>
+import Card from "@/components/Card";
+
+export default {
+  name: 'Home',
+  components: {
+    Card
+  }
+}
+</script>
+
+<style scoped>
+.time {
+  font-size: 5rem;
+  display: flex;
+  flex: 1;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+}
+
+.center {
+  display: flex;
+  height: 100%;
+}
+</style>
