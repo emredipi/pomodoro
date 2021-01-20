57bf4233d340675cee737f2748a7bd6805c92b47
emredipi <mail@emredipi.com>
2021-01-20 13:33:14 +0300

    added break and study titles

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,6 @@
 <template>
   <Card>
-    <div slot="header">Header</div>
+    <div slot="header" class="break">MOLA</div>
     <div class="center">
       <Timer/>
     </div>
@@ -10,6 +10,7 @@
 <script>
 import Card from "@/components/Card";
 import Timer from "@/components/Timer";
+
 export default {
   name: 'Home',
   components: {
@@ -22,6 +23,25 @@ export default {
 <style scoped>
 .center {
   display: flex;
+  flex-direction: column;
   height: 100%;
+}
+
+.break, .study {
+  border-radius: 5px 5px 0 0;
+  padding: 5px;
+  font-size: 2rem;
+  margin: -10px;
+  text-align: center;
+}
+
+.break {
+  background-color: #cdcd00;
+  color: black;
+}
+
+.study {
+  background-color: #00cd52;
+  color: black;
 }
 </style>
